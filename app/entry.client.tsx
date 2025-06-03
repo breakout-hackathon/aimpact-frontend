53f2c929f51d9300e98139a4726f50d3111db776
Alex Pitenko <oleksii.p@itsyndicate.org>
2025-06-03 22:08:39 +0200

    add sentry integration

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,27 @@
-import { RemixBrowser } from '@remix-run/react';
-import { startTransition } from 'react';
+import { init, replayIntegration, browserTracingIntegration } from "@sentry/remix";
+import { RemixBrowser, useLocation, useMatches } from '@remix-run/react';
+import { startTransition, useEffect } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { AppProvider } from '~/providers';
+
+init({
+    dsn: "https://3ce6d32dc3d38efe0423220e0772c4a6@o4509436444278784.ingest.de.sentry.io/4509436451618896",
+    tracesSampleRate: 0.1,
+
+    integrations: [browserTracingIntegration({
+      useEffect,
+      useLocation,
+      useMatches
+    }), replayIntegration({
+        maskAllText: true,
+        blockAllMedia: true
+    })],
+
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    sampleRate: 0.1,
+    environment: import.meta.env.ENVIRONMENT || 'development',
+})
 
 const root = document.getElementById('root')!;
 
